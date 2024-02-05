7a2afeff1712223c0bc8991b0f6da87d2073e259
Jivon Parker <123410317+jjparker34@users.noreply.github.com>
2024-02-04 21:40:28 -0600

    Start shiny app

--- Elite11_cfbfastr.R
+++ Elite11_cfbfastr.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(cfbfastR)
+library(shiny)
 
 Sys.setenv(CFBD_API_KEY = "S2JgCsvJ5RPuw2dDwPquZ1xTKefAk5UT/H0qeK9jZAAH46WMsGbKtfRJ4odTswoX")
 Elite11Players <- read_csv("Elite11Players.csv")
@@ -13,5 +13,35 @@ MVPs_after_2010 <- MVPs %>%
   filter(Year >= 2010)
 
 
-#player_info <- cfbd_player_info(search_term = player_name, position = position, team = team, year = year)
-#cfbd_player_info(search_term = "Berlin", position = "QB", team = "Florida", year = 2001)
+
+MVP_stats_avg <- read_csv("MVP_stats_avg.csv")
+MVP_stats_yearly <- read_csv("MVP_stats.csv")
+
+#shiny display
+ui <- fluidPage(
+  titlePanel("My Shiny App"),
+  sidebarLayout(
+    sidebarPanel(
+      # Input elements go here
+    ),
+    mainPanel(
+      # Output elements go here
+    )
+  )
+)
+
+server <- function(input, output) {
+  # Define reactive objects and functions here
+}
+
+# Example of a reactive object
+reactive_data <- reactive({
+  # Compute or process data based on user inputs
+  # Return the result
+})
+
+output$plot <- renderPlot({
+  # Generate a plot based on reactive data
+})
+
+shinyApp(ui = ui, server = server)
