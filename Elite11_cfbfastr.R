30d4a44404be63cf5e18876beb816e17ed548467
Jivon Parker <123410317+jjparker34@users.noreply.github.com>
2024-02-07 22:34:15 -0600

    Add files via upload

--- Elite11_cfbfastr.R
+++ Elite11_cfbfastr.R
@@ -1,8 +1,14 @@
 library(tidyverse)
 library(shiny)
+library(ggplot2)
+library(gt)
 
 Sys.setenv(CFBD_API_KEY = "S2JgCsvJ5RPuw2dDwPquZ1xTKefAk5UT/H0qeK9jZAAH46WMsGbKtfRJ4odTswoX")
 Elite11Players <- read_csv("Elite11Players.csv")
+#split elite 11 players into First and Last name
+name_parts <- strsplit(Elite11Players$Name, " ")
+Elite11Players$First_Name <- sapply(name_parts, function(x) ifelse(length(x) >= 2, x[1], x))
+Elite11Players$Last_Name <- sapply(name_parts, function(x) ifelse(length(x) >= 2, paste(x[-1], collapse = " "), ""))
 
 
 #Elite 11 MVPs
@@ -17,31 +23,19 @@ MVPs_after_2010 <- MVPs %>%
 MVP_stats_avg <- read_csv("MVP_stats_avg.csv")
 MVP_stats_yearly <- read_csv("MVP_stats.csv")
 
-#shiny display
-ui <- fluidPage(
-  titlePanel("My Shiny App"),
-  sidebarLayout(
-    sidebarPanel(
-      # Input elements go here
-    ),
-    mainPanel(
-      # Output elements go here
-    )
-  )
-)
+# Find MVP's best PPR year
 
-server <- function(input, output) {
-  # Define reactive objects and functions here
-}
+highest_ppr_year <- MVP_stats_yearly %>%
+  group_by(Player) %>%
+  top_n(1, `PPR Points`) %>%
+  select(Player,Team,Season,`Year out of HS`,`Games Played`,`PPR Points`,`PPR Points per Game`,`Draft Pick Overall`)
 
-# Example of a reactive object
-reactive_data <- reactive({
-  # Compute or process data based on user inputs
-  # Return the result
-})
 
-output$plot <- renderPlot({
-  # Generate a plot based on reactive data
-})
-
-shinyApp(ui = ui, server = server)
+highest_ppr_year %>%
+  gt() %>%
+  tab_header(
+    title = md('***Elite 11 MVPs Best Year (2010-2023)***'),
+    subtitle = md('Elite 11 MVP and their best year in college stats based on ppr points')
+  ) %>%
+  opt_align_table_header(align = 'left') 
+     
